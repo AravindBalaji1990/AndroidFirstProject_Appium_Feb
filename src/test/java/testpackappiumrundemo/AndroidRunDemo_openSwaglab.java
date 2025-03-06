2502a0412c3526196a33785a71f424f6de7d9766
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2025-03-06 20:07:26 +0300

    New Repo Feb

--- src/test/java/testpackappiumrundemo/AndroidRunDemo_openSwaglab.java
+++ src/test/java/testpackappiumrundemo/AndroidRunDemo_openSwaglab.java
@@ -7,15 +7,20 @@ import io.appium.java_client.remote.AutomationName;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class AndroidRunDemo {
+public class AndroidRunDemo_openSwaglab {
+
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
-        // install the app and open the app
+        // opne the app via main activity and package name
         //this is configuration only for Android
         UiAutomator2Options obj_andorid = new UiAutomator2Options();
         obj_andorid.setPlatformName("Android");
         obj_andorid.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
         obj_andorid.setUdid("29221JEGR00379");
-        obj_andorid.setApp(System.getProperty("user.dir")+"\\src\\test\\resources\\sampleapp\\Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
+//        obj_andorid.setAppPackage("com.swaglabsmobileapp");
+//        obj_andorid.setAppActivity("com.swaglabsmobileapp.MainActivity");
+        obj_andorid.setAppPackage("com.flipkart.android");
+//        obj_andorid.setAppActivity("com.flipkart.youtubeview.activity.YouTubeActivity - Flipkart");// wrong one
+        obj_andorid.setAppActivity("com.flipkart.android.activity.HomeFragmentHolderActivity");// wrong one
 
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"),obj_andorid);
 
