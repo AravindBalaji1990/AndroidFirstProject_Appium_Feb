f02fcc555fa141832c5b9be4d1474436e6ee8e12
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2025-03-19 19:57:59 +0300

    test lcoators

--- src/test/java/testpackappiumiosdemo/IosRunDemo_ExistingApp_IosSpecific.java
+++ src/test/java/testpackappiumiosdemo/IosRunDemo_ExistingApp_IosSpecific.java
@@ -8,23 +8,26 @@ import io.appium.java_client.remote.AutomationName;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class IosRunDemo_ExistingAPpp {
+public class IosRunDemo_ExistingApp_IosSpecific {
 
     public static void main(String[] args) throws InterruptedException, MalformedURLException {
         XCUITestOptions object_XCUITestOptions = new XCUITestOptions();
         object_XCUITestOptions.setPlatformName("IOS");
         object_XCUITestOptions.setAutomationName(AutomationName.IOS_XCUI_TEST);
 
-//        object_XCUITestOptions.setBundleId("com.moataz.dailycheck");
-        object_XCUITestOptions.setApp("/Users/aravindbalaji/Documents/Appium/SampleApp/ios-app.zip");
+        object_XCUITestOptions.setBundleId("com.moataz.dailycheck");
 
         object_XCUITestOptions.setUdid("08BBF8F2-37A6-42E0-AB9F-65C5E6AEC31B");
-//        object_XCUITestOptions.setDeviceName("iPhone 16 Plus");
+
 
         IOSDriver driver = new IOSDriver(new URL("http://127.0.0.1:4723/"), object_XCUITestOptions);
         Thread.sleep(3000);
 
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeButton[`name == \"plus.circle\"`]")).click();
+        Thread.sleep(3000);
 
+
+        System.out.println(driver.findElement(AppiumBy.iOSNsPredicateString("name == \"Scheduled\"")).isDisplayed());
         driver.quit();
 
     }
